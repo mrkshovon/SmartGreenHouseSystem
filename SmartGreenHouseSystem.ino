57fc33894050f2e737a90ecb608d47d4f3a672c4
mrkshovon <mrkshovon@gmail.com>
2020-12-10 22:20:33 +0000

    Add files via upload

---
+++ SmartGreenHouseSystem.ino
@@ -0,0 +1,372 @@
+#include <LittleFS.h>
+#include <ESP8266WiFi.h>
+#include <time.h>
+//#include <Adafruit_NeoPixel.h>
+#include <DHT.h>
+#include <ArduinoJson.h>
+#include <PubSubClient.h>
+#include <math.h>
+// two libraries for MPL3115 sensor
+#include <Wire.h>
+#include <Adafruit_MPL3115A2.h>
+// --------------------------------------------------------------------------------------------
+//        UPDATE CONFIGURATION TO MATCH YOUR ENVIRONMENT
+// --------------------------------------------------------------------------------------------
+
+// Watson IoT connection details
+#define MQTT_HOST "lcbzzn.messaging.internetofthings.ibmcloud.com"
+#define MQTT_PORT 8883
+#define MQTT_DEVICEID "d:lcbzzn:ESP8266:dev01"
+#define MQTT_USER "use-token-auth"
+#define MQTT_TOKEN "edinburgh2020"
+#define MQTT_TOPIC "iot-2/evt/status/fmt/json"
+#define MQTT_TOPIC_DISPLAY "iot-2/cmd/display/fmt/json"
+#define MQTT_TOPIC_INTERVAL "iot-2/cmd/interval/fmt/json"
+#define MQTT_TOPIC_TEMP "iot-2/cmd/temperature/fmt/json" //added
+#define MQTT_TOPIC_HUMIDITY "iot-2/cmd/humidity/fmt/json" //added
+#define CA_CERT_FILE "/rootCA_certificate.pem"
+#define KEY_FILE "/SecuredDev01_key_nopass.pem"
+#define CERT_FILE "/SecuredDev01_crt.pem"
+
+// Add GPIO pins used to connect devices
+//#define RGB_PIN 5 // GPIO pin the data line of RGB LED is connected to
+#define DHT_PIN 15 // GPIO pin the data line of the DHT sensor is connected to
+
+// Specify DHT11 (Blue) or DHT22 (White) sensor
+//#define DHTTYPE DHT22
+#define DHTTYPE DHT11
+//#define NEOPIXEL_TYPE NEO_RGB + NEO_KHZ800
+
+// Temperatures to set LED by (assume temp in C)
+#define ALARM_COLD 0.0
+#define ALARM_HOT 30.0
+#define WARN_COLD 10.0
+#define WARN_HOT 25.0
+
+//Timezone info
+#define TZ_OFFSET 0  //Hours timezone offset to GMT (without daylight saving time)
+#define TZ_DST    60  //Minutes timezone offset for Daylight saving
+#define humidifier 12
+#define heater 13
+#define fan 14
+Adafruit_MPL3115A2 baro = Adafruit_MPL3115A2();
+
+// Add WiFi connection information
+char ssid[] = "SHOVON";  // your network SSID (name)
+char pass[] = "12345678";  // your network password
+
+// Model parameters from part4 - to implement the model on the ESP8266
+// Replace these parameters with the model parameters from your Jupyter Notebook
+#define MODEL_INTERCEPT -14.172253183961212
+#define MODEL_TEMP_COEF -3.0625
+#define MODEL_HUM_COEF 1.3247
+
+// --------------------------------------------------------------------------------------------
+//        SHOULD NOT NEED TO CHANGE ANYTHING BELOW THIS LINE
+// --------------------------------------------------------------------------------------------
+//Adafruit_NeoPixel pixel = Adafruit_NeoPixel(1, RGB_PIN, NEOPIXEL_TYPE);
+DHT dht(DHT_PIN, DHTTYPE);
+
+// MQTT objects
+void callback(char* topic, byte* payload, unsigned int length);
+BearSSL::WiFiClientSecure wifiClient;
+PubSubClient mqtt(MQTT_HOST, MQTT_PORT, callback, wifiClient);
+
+BearSSL::X509List *rootCert;
+BearSSL::X509List *clientCert;
+BearSSL::PrivateKey *clientKey;
+
+// variables to hold data
+StaticJsonDocument<100> jsonDoc;
+JsonObject payload = jsonDoc.to<JsonObject>();
+JsonObject status = payload.createNestedObject("d");
+StaticJsonDocument<100> jsonReceiveDoc;
+static char msg[100];
+
+float h = 0.0; // humidity
+float t = 0.0; // temperature
+ bool r = 0; // LED RED value
+ bool g = 0; // LED Green value
+ bool b = 0; // LED Blue value
+ bool tt = 0; 
+ bool y = 0; 
+int32_t ReportingInterval = 10;  // Reporting Interval seconds
+float set_temp = 20;
+float set_humidity= 40;
+float pascals=0;
+int altm=0;
+float tempc=0;
+
+float applyModel(float h, float t) {
+  // apply regression formula w1 + w2x + w3y
+  float regression = MODEL_INTERCEPT + MODEL_HUM_COEF * h + MODEL_TEMP_COEF * t;
+  // return sigmoid logistic function on regression result
+  return  1/(1 + exp(0.0 - (double)regression));
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  // handle message arrived
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] : ");
+
+  payload[length] = 0; // ensure valid content is zero terminated so can treat as c-string
+  Serial.println((char *)payload);
+  DeserializationError err = deserializeJson(jsonReceiveDoc, (char *)payload);
+  if (err) {
+    Serial.print(F("deserializeJson() failed with code "));
+    Serial.println(err.c_str());
+  } else {
+    JsonObject cmdData = jsonReceiveDoc.as<JsonObject>();
+    if (0 == strcmp(topic, MQTT_TOPIC_DISPLAY)) {
+      //valid message received
+      r = cmdData["r"].as<bool>();
+      g = cmdData["g"].as<bool>();
+      b = cmdData["b"].as<bool>();
+      tt = cmdData["t"].as<bool>();//mod
+      y = cmdData["y"].as<bool>();//
+      jsonReceiveDoc.clear();
+      digitalWrite(16,r);
+      //digitalWrite(5,g);
+      digitalWrite(0,b);
+      digitalWrite(2,tt);
+     // digitalWrite(14,y);
+     // pixel.setPixelColor(0, r, g, b);
+      //pixel.show();
+    } else if (0 == strcmp(topic, MQTT_TOPIC_INTERVAL)) {
+      //valid message received
+      ReportingInterval = cmdData["Interval"].as<int32_t>(); // this form allows you specify the type of the data you want from the JSON object
+      Serial.print("Reporting Interval has been changed:");
+      Serial.println(ReportingInterval);
+      jsonReceiveDoc.clear();
+    } else if (0 == strcmp(topic, MQTT_TOPIC_TEMP)) {
+      //valid message received
+      set_temp = cmdData["temperature"].as<float>();
+      Serial.print("Temperature has been changed: ");
+      Serial.println(set_temp);
+      jsonReceiveDoc.clear();
+    } else if (0 == strcmp(topic, MQTT_TOPIC_HUMIDITY)) {
+      //valid message received
+      set_humidity = cmdData["Humidity"].as<float>();
+      Serial.print("Humidity has been changed: ");
+      Serial.println(set_humidity);
+      jsonReceiveDoc.clear();
+    } else {
+      Serial.println("Unknown command received");
+    }
+  }
+}
+
+
+
+void setup() {
+  char *ca_cert = nullptr;
+  char *client_cert = nullptr;
+  char *client_key = nullptr;
+  /// Mod
+  pinMode(16, OUTPUT);
+ // pinMode(5, OUTPUT);
+  pinMode(0, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(fan, OUTPUT);
+  pinMode (humidifier,OUTPUT);
+  pinMode (heater,OUTPUT);
+  /// Mod
+  // Start serial console
+  Serial.begin(115200);
+  Serial.setTimeout(2000);
+  while (!Serial) { }
+  Serial.println();
+  Serial.println("ESP8266 Sensor Application");
+
+  // Start WiFi connection
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi Connected");
+
+  // Start connected devices
+  dht.begin();
+ // pixel.begin();
+
+  // Get cert(s) from file system
+  LittleFS.begin();
+  File ca = LittleFS.open(CA_CERT_FILE, "r");
+  if(!ca) {
+    Serial.println("Couldn't load CA cert");
+  } else {
+    size_t certSize = ca.size();
+    ca_cert = (char *)malloc(certSize);
+    if (certSize != ca.readBytes(ca_cert, certSize)) {
+      Serial.println("Loading CA cert failed");
+    } else {
+      Serial.println("Loaded CA cert");
+      rootCert = new BearSSL::X509List(ca_cert);
+      wifiClient.setTrustAnchors(rootCert);
+    }
+    free(ca_cert);
+    ca.close();
+  }
+
+  File key = LittleFS.open(KEY_FILE, "r");
+  if(!key) {
+    Serial.println("Couldn't load key");
+  } else {
+    size_t keySize = key.size();
+    client_key = (char *)malloc(keySize);
+    if (keySize != key.readBytes(client_key, keySize)) {
+      Serial.println("Loading key failed");
+    } else {
+      Serial.println("Loaded key");
+      clientKey = new BearSSL::PrivateKey(client_key);
+    }
+    free(client_key);
+    key.close();
+  }
+
+  File cert = LittleFS.open(CERT_FILE, "r");
+  if(!cert) {
+    Serial.println("Couldn't load cert");
+  } else {
+    size_t certSize = cert.size();
+    client_cert = (char *)malloc(certSize);
+    if (certSize != cert.readBytes(client_cert, certSize)) {
+      Serial.println("Loading client cert failed");
+    } else {
+      Serial.println("Loaded client cert");
+      clientCert = new BearSSL::X509List(client_cert);
+    }
+    free(client_cert);
+    cert.close();
+  }
+
+  wifiClient.setClientRSACert(clientCert, clientKey);
+
+  // Set time from NTP servers
+  configTime(TZ_OFFSET * 3600, TZ_DST * 60, "1.pool.ntp.org", "0.pool.ntp.org");
+  Serial.println("\nWaiting for time");
+  unsigned timeout = 30000;
+  unsigned start = millis();
+  while (millis() - start < timeout) {
+      time_t now = time(nullptr);
+      if (now > (2018 - 1970) * 365 * 24 * 3600) {
+          break;
+      }
+      delay(100);
+  }
+  delay(1000); // Wait for time to fully sync
+  Serial.println("Time sync'd");
+  time_t now = time(nullptr);
+  Serial.println(ctime(&now));
+
+  // Connect to MQTT - IBM Watson IoT Platform
+   while(! mqtt.connected()){
+    if (mqtt.connect(MQTT_DEVICEID, MQTT_USER, MQTT_TOKEN)) { // Token Authentication
+//    if (mqtt.connect(MQTT_DEVICEID)) { // No Token Authentication
+      Serial.println("MQTT Connected");
+      mqtt.subscribe(MQTT_TOPIC_DISPLAY);
+      mqtt.subscribe(MQTT_TOPIC_INTERVAL);
+      mqtt.subscribe(MQTT_TOPIC_TEMP);
+      mqtt.subscribe(MQTT_TOPIC_HUMIDITY);
+    } else {
+      Serial.print("last SSL Error = ");
+      Serial.print(wifiClient.getLastSSLError(msg, 100));
+      Serial.print(" : ");
+      Serial.println(msg);
+      Serial.println("MQTT Failed to connect! ... retrying");
+      delay(500);
+    }
+  }
+}
+
+
+void loop() {
+  mqtt.loop();
+  while (!mqtt.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (mqtt.connect(MQTT_DEVICEID, MQTT_USER, MQTT_TOKEN)) { // Token Authentication
+//    if (mqtt.connect(MQTT_DEVICEID)) { // No Token Authentication
+      Serial.println("MQTT Connected");
+      mqtt.subscribe(MQTT_TOPIC_DISPLAY);
+      mqtt.subscribe(MQTT_TOPIC_INTERVAL);
+      mqtt.subscribe(MQTT_TOPIC_TEMP);
+       mqtt.subscribe(MQTT_TOPIC_HUMIDITY);
+      mqtt.loop();
+    } else {
+      Serial.print("last SSL Error = ");
+      Serial.print(wifiClient.getLastSSLError(msg, 100));
+      Serial.print(" : ");
+      Serial.println(msg);
+      Serial.println("MQTT Failed to connect!");
+      delay(5000);
+    }
+  }
+if (! baro.begin()) {
+    Serial.println("Couldnt find sensor");
+ }
+  h = dht.readHumidity();
+  t = dht.readTemperature(); // uncomment this line for Celsius
+  // t = dht.readTemperature(true); // uncomment this line for Fahrenheit
+   pascals = baro.getPressure();
+   pascals = pascals/3377;
+    altm = baro.getAltitude();
+    tempc = baro.getTemperature();
+ 
+ 
+ 
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+  } else {
+ 
+     // Apply the model to the sensor readings
+    float modelPrediction = applyModel(h, t);
+
+    // Print Message to console in JSON format
+    status["temp"] = t;
+    status["humidity"] = h;
+    status["Pressure"] = pascals;
+    status["Altitude"] = altm;
+    status["Temp_MPL3115A2"] = tempc;
+    Serial.print("Model output = ");
+    Serial.println(modelPrediction);
+    status["class"] = modelPrediction < 0.5 ? 0 : 1;
+    serializeJson(jsonDoc, msg, 100);
+    Serial.println(msg);
+    if (!mqtt.publish(MQTT_TOPIC, msg)) {
+      Serial.println("MQTT Publish failed");
+    }
+  }
+
+  Serial.print("Data transmission Interval :");
+  Serial.print(ReportingInterval);
+  Serial.println();
+  Serial.print("Temperature is set to:");//added
+  Serial.print(set_temp);//added
+  Serial.println();//added
+  Serial.print("Humidity is set to :");//added
+  Serial.print(set_humidity);//added
+  Serial.println();//added
+
+  if (set_humidity > h){
+    digitalWrite(humidifier,HIGH);
+    } else if (set_humidity < h)
+    {
+      digitalWrite(humidifier,LOW);
+      }
+      if (set_temp > t){
+    digitalWrite(heater,HIGH);
+    } else if (set_temp < t){
+      digitalWrite(heater,LOW);
+      }
+  // Pause - but keep polling MQTT for incoming messages
+  for (int32_t i = 0; i < ReportingInterval; i++) {
+    mqtt.loop();
+    delay(1000);
+  }
+}
